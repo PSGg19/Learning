76aa89fe632196a8575347b60a8408dffe2b4c68
PSGg19 <prahladsharan19@gmail.com>
2025-03-12 14:25:14 +0530

    javascript bhi seekh liya

---
+++ Html/index.js
@@ -0,0 +1,25 @@
+console.log(7);
+var a=7;
+if(a===7)
+{
+    console.log("han bhai ghum liya");
+}
+else
+{
+    console.log("aisa kuch nhi hai");
+}
+let v1=5;
+{
+    let v=5;
+    {
+    console.log(v);
+    }
+    
+}
+
+{
+    console.log(a);
+}
+let a1=3;
+let b=5;
+console.log(a1**b);
